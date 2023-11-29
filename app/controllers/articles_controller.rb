34e3d4780b928d537959a1439e16effb877a6e33
Aquibe <aquib1127@gmail.comm>
2023-11-29 14:00:52 +0530

    Add users table and user model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    # @article = Article.find(params[:id]) //DRY method
+
   end
   def index
     @articles = Article.all
@@ -12,20 +12,20 @@ class ArticlesController < ApplicationController
     @article=Article.new
   end
   def edit
-    # @article = Article.find(params[:id])//DRY method
+
   end
   def create
-    @article = Article.new(article_params)#(params.require(:article).permit(:title, :description))//DRY method
+    @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
-      flash[:notice] = "Article was create successfully."
+      flash[:notice] = "Article was created successfully."
       redirect_to @article
     else
       render 'new'
     end
   end
   def update
-    # @article= Article.find(params[:id])//DRY method
-    if @article.update(article_params)#(params.require(:article).permit(:title, :description))//DRY method
+    if @article.update(article_params)
      flash[:notice]= "Article was updated successfully"
      redirect_to @article
     else
@@ -33,13 +33,11 @@ class ArticlesController < ApplicationController
     end
   end
   def destroy
-    # @article= Article.find(params[:id])// DRY method
     @article.destroy
     redirect_to articles_path
   end
 
   private
-
   def set_article
     @article = Article.find(params[:id])
   end
@@ -47,5 +45,4 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :description)
   end
-
 end
