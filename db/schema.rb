17e0727de36cf6c8098c4eedb09a0370fef300ee
Aquibe <aquib1127@gmail.comm>
2023-12-04 15:54:51 +0530

    Add edit template and update action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema[7.1].define(version: 2023_11_13_111207) do
+=======
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_064921) do
+>>>>>>> users-signup
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_13_111207) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
